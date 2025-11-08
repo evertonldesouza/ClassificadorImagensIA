21fa2d5575b657a9f4489916f28daf74fdf96fca
Everton L. de Souza <evertonldesouza@proton.me>
2025-11-08 15:14:27 -0300

    .

--- service-worker.js
+++ service-worker.js
@@ -3,8 +3,8 @@ const CACHE_NAME = 'classificador-cache-v2';
 const FILES_TO_CACHE = [
   '/',
   '/index.html',
-  '/Assets/css/style.css',
-  '/Assets/js/script.js',
+  './Assets/css/style.css',
+  './Assets/js/script.js',
   'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@latest/dist/tf.min.js',
   'https://cdn.jsdelivr.net/npm/@tensorflow-models/mobilenet@latest/dist/mobilenet.min.js',
   'https://cdn.jsdelivr.net/npm/@tensorflow-models/coco-ssd@latest/dist/coco-ssd.min.js'
